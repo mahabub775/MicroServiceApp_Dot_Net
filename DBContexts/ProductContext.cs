bb9629023a3e4e4df152e8f56c2ac7cd3ec5f2e6
Mahabub <mahabub775@gmail.com>
2021-02-17 08:09:30 +0600

    First commit

---
+++ DBContexts/ProductContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductMicroService.Models;
+
+namespace ProductMicroService.DBContexts
+{
+        public class ProductContext : DbContext
+        {
+            public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+            {
+
+            }
+            public DbSet<Product> Products { get; set; }
+          public DbSet<Category> Categories { get; set; }
+
+        //it will be create when will first time run
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Electronics",
+                    Description = "Electronic Items",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Clothes",
+                    Description = "Dresses",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Grocery",
+                    Description = "Grocery Items",
+                }
+            );
+        }
+
+    }
+}
---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductMicroService.Models;
+
+namespace ProductMicroService.Repository
+{
+    interface IProductRepository
+    {
+
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int ProductId);
+        void InsertProduct(Product Product);
+        void UpdateProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void Save();
+
+    }
+}
